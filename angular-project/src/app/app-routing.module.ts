cd381483a4a892a9fcb1df8ff798a5aa8e4777be
boby4a <52974503+BorislavChavdarov@users.noreply.github.com>
2022-04-06 19:47:31 +0300

    add home, register, login page

--- angular-project/src/app/app-routing.module.ts
+++ angular-project/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
+import { HomeComponent } from "./shared/home/home.component"
+const routes: Routes = [
+  {path:"home", component: HomeComponent},
+  {path:"login", component: LoginComponent},
+  {path:"register", component: RegisterComponent},
+  {path:"about-us", component: AboutUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './shared/home/home.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AboutUsComponent } from './shared/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
